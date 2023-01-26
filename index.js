ed4f0121251f523745664cfc5090f92c6fc197c8
DanielGamma <daniel.carrasco@gammatech.school>
2023-01-26 16:08:23 +0100

    Undo last swagger changes

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/docs', swaggerUI.serve);
-app.get('/api/docs', swaggerUI.setup(specs));
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use('/api/products', router);
 
--- swagger/swagger_config.js
+++ swagger/swagger_config.js
@@ -14,7 +14,6 @@ const options = {
                 // url: "http://localhost:3001/api/products"
             }
         ],
-        produces: ['application/json']
     },
     apis: ["./swagger/swagger_products.js"]
 }
